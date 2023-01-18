df5cc1366774baca6d791104e766be8b18c16ee2
Tom Booth <tombeezle@gmail.com>
2023-01-18 14:58:31 -0800

    continents filtering with try catch, css changed cause react didnt like something

--- src/components/hooks/useCountries.js
+++ src/components/hooks/useCountries.js
@@ -3,14 +3,24 @@ import { fetchCountries } from '../../services/countries';
 
 export function useCountries() {
   const [countries, setCountries] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     async function fetchData() {
-      const data = await fetchCountries();
-      setCountries(data);
+      try {
+        const data = await fetchCountries();
+        data.map((country) => {
+          if (!country.continent) {
+            country.continent = 'none';
+          }
+        });
+        setCountries(data);
+      } catch (e) {
+        setError('something went wrong');
+      }
     }
     fetchData();
   }, []);
 
-  return { countries };
+  return { countries, error };
 }
