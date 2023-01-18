34c9192780d124aa1750d9252789782d3142ab93
Tom Booth <tombeezle@gmail.com>
2023-01-18 15:05:28 -0800

    added real names to header and footer

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './footer.css';
 
 export default function footer() {
-  return <footer>footer</footer>;
+  return <footer>Toms Country Site 2023</footer>;
 }
--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './header.css';
 
 export default function header() {
-  return <header>Header</header>;
+  return <header>Countries of the WORLD! with Flags!</header>;
 }
